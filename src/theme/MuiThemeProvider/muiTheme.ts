6957abc1887ca4c91257fed26175c112b853379e
Corey Rice <coreyarice@gmail.com>
2022-03-14 12:49:07 -0300

    Move global switch styles to component

--- src/theme/MuiThemeProvider/muiTheme.ts
+++ src/theme/MuiThemeProvider/muiTheme.ts
@@ -191,21 +191,5 @@ export default createTheme({
         },
       },
     },
-    MuiSwitch: {
-      styleOverrides: {
-        switchBase: {
-          color: PALETTE.text.disabled,
-          '&.Mui-checked': {
-            color: PALETTE.interactive.error,
-          },
-          '&+.MuiSwitch-track': {
-            backgroundColor: PALETTE.background.default,
-          },
-          '&.Mui-checked+.MuiSwitch-track': {
-            backgroundColor: PALETTE.background.default,
-          },
-        },
-      },
-    },
   },
 } as ThemeOptions);
